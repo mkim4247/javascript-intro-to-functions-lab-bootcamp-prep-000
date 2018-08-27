0713b551d486c699b229f1a628887df52417105e
Michael Kim <m.kim4247@gmail.com>
2018-08-27 22:22:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ function logWhisper(string) {
   console.log("hello")
 }
 function sayHiToGrandma (string) {
-  if (string === "I love you, Grandma") {
-  return("I love you, too!")
-} else if (string === "HELLO!") {
-  return "YES INDEED!"
-} else if (string === "hello!") {
-  return "I can't hear you!"
-}
-}
+  if (string = "hello") {
+    return "I can't hear you!"
+    } else if (string = "HELLO!") {
+      return "YES INDEED!"
+    } else if (string = "I love you, Grandma!") {
+      return "I love you, too!"
+    }
+  }
